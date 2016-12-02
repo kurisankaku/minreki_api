11f3c850ef93a35f272e0e700bfff7454f52987f
Kuriyama Munehisa <kurisankaku@gmail.com>
2016-12-02 18:13:42 +0900

    Fix bug

--- app/api/minreki/accounts_api.rb
+++ app/api/minreki/accounts_api.rb
@@ -2,11 +2,11 @@ module Minreki
   # Account API class.
   class AccountsAPI < Grape::API
     include ApiSettings
+
     resource :accounts do
-      version 'v1' do
-        get '/' do
-          'Hello World'
-        end
+      get '/' do
+        'Hello World'
+      end
     end
   end
 end
