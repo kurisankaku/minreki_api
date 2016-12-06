0de950914f57cc2876dd314c709f0bed1c9095b6
Kuriyama Munehisa <kurisankaku@gmail.com>
2016-12-06 19:12:48 +0900

    Add accounts routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, skip: :all
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :accounts, only: [:create, :show, :destroy] do
+    resource :name, only: [:update]
+    resource :password, only: [:update]
+    resource :email, only: [:update]
+  end
 end
