2de3d292399a510abcb5a28596f1a43da31a603b
Kuriyama Munehisa <kurisankaku@gmail.com>
2016-12-07 18:43:39 +0900

    Add devise_scope to routes for using Devsie modules.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users, skip: :all
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  resources :accounts, only: [:create, :show, :destroy] do
-    resource :name, only: [:update]
-    resource :password, only: [:update]
-    resource :email, only: [:update]
+  devise_scope :user do
+    resources :accounts, only: [:create, :show, :destroy] do
+      resource :name, only: [:update]
+      resource :password, only: [:update]
+      resource :email, only: [:update]
+    end
   end
+  devise_for :users, only: :confirmation, controllers: { confirmations: 'accounts/confirmations'}
 end
