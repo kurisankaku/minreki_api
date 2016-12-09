1c42f86e563b66dcfaff08144318019383e5a3d2
Kuriyama Munehisa <kurisankaku@gmail.com>
2016-12-09 17:50:51 +0900

    Modify auth confirmation action.

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -1,14 +1,21 @@
 # Confirmation token api.
 class ConfirmationsController < ApplicationController
-  include DeviseUtils
+  include AuthenticationUtils
 
   def show
-    self.resource = resource_class.confirm_by_token(params[:id])
-    if resource.errors.empty?
+    self.resource = resource_class.confirm_by_token(params[:token])
+    set_user_id_to_params(resource)
+
+    # TODO: must refactor this logic.
+    request.parameters[:client_id] = "4a87f6ed247e74473af1e91280e756f99bc707e32637042431d56d8848a871fe"
+    params[:redirect_uri] = "http://minreki.com"
+    params[:response_type] = "token"
+
+    # if resource.errors.empty?
       result = authorization.authorize
-      render json: result.auth, status: 201
-    else
-      fail
-    end
+      render json: result.auth.token, status: 200
+    # else
+    #   fail
+    # end
   end
 end
