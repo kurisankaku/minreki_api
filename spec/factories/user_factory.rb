9ef38979f13c07d053672f28647092aecf4114fb
Kuriyama Munehisa <kurisankaku@gmail.com>
2016-12-12 10:19:23 +0900

    Require securerandom to spec for generating id

--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -1,5 +1,6 @@
 FactoryGirl.define do
   factory :user do
+    id { SecureRandom.hex(64) }
     name { Faker::Name.name }
     email { Faker::Internet.email }
     password { "1a" + Faker::Internet.password(6) }
